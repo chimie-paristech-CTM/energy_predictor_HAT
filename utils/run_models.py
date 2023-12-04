06696dc7f9512177ff956d34cae60628a14c1ee2
Javier Emilio Alfonso Ramos <javialra97@gmail.com>
2023-12-04 19:45:15 +0100

    full pipeline

---
+++ utils/run_models.py
@@ -0,0 +1,32 @@
+import os
+import subprocess
+
+def run_surrogate():
+
+        path = f"surrogate_model/predict.py"
+        test_path = "tmp/species_reactivity_dataset.csv"
+        chk_path = "surrogate_model/qmdesc_wrap/model.pt"
+        preds_path = "tmp/preds_surrogate.pkl"
+        inputs = f"--test_path {test_path} --checkpoint_path {chk_path} --preds_path {preds_path}"
+
+        with open('out_file', 'w') as out:
+            subprocess.run(f"python {path} {inputs}", shell=True, stdout=out, stderr=out)
+
+        return None
+
+
+def run_reactivity():
+
+    path = f"reactivity_model/predict.py"
+    pred_file = "tmp/input_ffnn.csv"
+    trained_dir = "reactivity_model/final_model_4/"
+    save_dir = "tmp/"
+    inputs = f"--pred_file {pred_file} --trained_dir {trained_dir} --save_dir {save_dir} --ensemble_size 4"
+
+    with open('out_file', 'w') as out:
+        subprocess.run(f"python {path} {inputs}", shell=True, stdout=out, stderr=out)
+
+    return None
+
+
+
